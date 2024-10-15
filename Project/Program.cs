3b946ffc7112789c81bbfd27a3a900a917fc9cd8
DavidOkh <1069745@hr.nl>
2024-10-15 11:35:09 +0200

    Create Admin Login
    Login for admin is
    username: admin
    password: adminpass
    
    Login for user is
    username: user1
    password: user1pass

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to this amazing program");
-Menu.Start();
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var loginService = new LoginService();
+        Console.WriteLine("Enter username:");
+        string username = Console.ReadLine();
+        Console.WriteLine("Enter password:");
+        string password = Console.ReadLine();
+
+        var user = loginService.Login(username, password);
+        if (user != null)
+        {
+            if (user.IsAdmin)
+            {
+                var adminMenu = new AdminMenu();
+                adminMenu.Show();
+            }
+            else
+            {
+                var userMenu = new UserMenu();
+                userMenu.Show();
+            }
+        }
+    }
+}
