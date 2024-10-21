03245ef96fad32ae752a475d9df87f18c2a0cbc9
Mvdv00 <matthijsvorm@gmail.com>
2024-10-21 14:55:44 +0200

    added simple class

---
+++ Project/Presentation/reservation.cs
@@ -0,0 +1,18 @@
+static class Reservation
+{
+    public static void Start()
+    {
+        // do
+    }
+
+    public static MovieModel PickMovie()
+    {
+        return null;
+        // show the user all tbe movies and let them pick one
+    }
+
+    public static void PickDay()
+    {
+
+    }
+}
