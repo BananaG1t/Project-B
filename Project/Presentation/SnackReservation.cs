a9dcf168dd7ed0cc970bb5ff576212e112a66781
Eljean-23 <88048873+Eljean-23@users.noreply.github.com>
2024-11-20 15:54:21 +0100

    ValidDouble method added

--- Project/Presentation/SnackReservation.cs
+++ Project/Presentation/SnackReservation.cs
@@ -3,15 +3,14 @@ static class SnackReservation
     public static void ManageSnacks()
     {
         string snackName;
-        float price;
+        double price;
         do
         {
             Console.WriteLine("What snack would you like to add");
             snackName = Console.ReadLine();
             if (snackName == "1") { Menu.AdminMenu(); }
-            Console.WriteLine("What is the price of the snack");
-            price = float.Parse(Console.ReadLine());
-            if (price == 1) { Menu.AdminMenu(); }
+
+            price = ValidDouble();
 
             if (snackName == null) { Console.WriteLine("The Snack name is invalid"); }
             else if (price < 0) { Console.WriteLine("The price is incorrect"); }
@@ -21,5 +20,27 @@ static class SnackReservation
                 Console.WriteLine("snack added");
             }
         } while (snackName != null & price < 0 );
+    }
+
+    private static double ValidDouble()
+    {
+        double price = 0;
+        bool valid = false;
+        while (!valid)
+        {
+            Console.WriteLine("What is the price of the snack (0,0): ");
+            string input = Console.ReadLine();
+
+            if (double.TryParse(input, out price))
+            {
+                valid = true;
+            }
+            else
+            {
+                General.PrintInRed("Invalid input. Please try again");
+            }
+        }
+
+        return price;
     }
 }
