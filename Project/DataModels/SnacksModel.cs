ffdd2dfb9b367e3e5b032cceb9eb5a1d86d37fd9
Jules-sudo <1084973@hr.nl>
2024-11-20 13:32:00 +0100

    some changes

--- Project/DataModels/SnacksModel.cs
+++ Project/DataModels/SnacksModel.cs
@@ -1,8 +1,17 @@
+using System.Data.Common;
+
 public class SnacksModel
 {
     public string Name { get; set; }
     public float Price { get; set; }
-    
+    public Int64 Id { get; set; }
+    public SnacksModel(Int64 id ,string name, float price)
+    {
+        Id = id;
+        Name = name;
+        Price = price; 
+    }
+
     public SnacksModel(string name, float price)
     {
         Name = name;
