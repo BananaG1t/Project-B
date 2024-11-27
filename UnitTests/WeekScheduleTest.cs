d85d4b028b6632b9fa0780a867a3302a2fb2c8d2
DavidOkh <1069745@hr.nl>
2024-11-27 09:09:17 +0100

    updated unittest

--- UnitTests/WeekScheduleTest.cs
+++ UnitTests/WeekScheduleTest.cs
@@ -1,32 +1,25 @@
 namespace UnitTests;
 
 [TestClass]
-public class ValidAccName
+public class MovieLogicTests
 {
     [TestMethod]
-
-    public class MovieLogicTests
+    public void IsAvailable_ReturnsTrue_WhenRoomIsAvailable()
     {
-        public void IsAvailable_ReturnsTrue_WhenRoomIsAvailable()
-        {
-            int room = 1;
-            DateTime startTime = new DateTime(2024, 11, 25, 10, 0, 0);
-            DateTime endTime = startTime.AddHours(2); 
+        int room = 1;
+        DateTime startTime = new DateTime(2024, 11, 25, 10, 0, 0);
+        DateTime endTime = startTime.AddHours(2); 
 
-            bool result = MovieLogic.IsAvailable(room, startTime, endTime);
+        Assert.True(MovieLogic.IsAvailable(room, startTime, endTime));
+    }
 
-            Assert.True(result);
-        }
+    [TestMethod]
+    public void IsAvailable_ReturnsFalse_WhenRoomIsUnavailable()
+    {
+        int room = 2;
+        DateTime startTime = new DateTime(2024, 11, 25, 14, 0, 0); 
+        DateTime endTime = startTime.AddHours(2); 
 
-        public void IsAvailable_ReturnsFalse_WhenRoomIsUnavailable()
-        {
-            int room = 2;
-            DateTime startTime = new DateTime(2024, 11, 25, 14, 0, 0); 
-            DateTime endTime = startTime.AddHours(2); 
-
-            bool result = MovieLogic.IsAvailable(room, startTime, endTime);
-
-            Assert.False(result);
-        }
+        Assert.False(MovieLogic.IsAvailable(room, startTime, endTime));
     }
 }
