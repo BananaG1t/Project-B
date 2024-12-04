6e0527e78af59f67f682a908b3a3d8f1765eda77
Mvdv00 <matthijsvorm@gmail.com>
2024-12-04 15:08:43 +0100

    Create CreateRoles.cs

---
+++ Project/Presentation/CreateRoles.cs
@@ -0,0 +1,9 @@
+public static class CreateRoles
+{
+    public static void AddRole()
+    {
+        string text = "What is the name of the role? ";
+
+        string roleName = Console.ReadLine();
+    }
+}
