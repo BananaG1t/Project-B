80a23e98eaed3aea83dc0e55c3441f37b8ac82cd
Mvdv00 <matthijsvorm@gmail.com>
2024-12-04 15:37:26 +0100

    fixed the db

--- Project/Presentation/CreateRoles.cs
+++ Project/Presentation/CreateRoles.cs
@@ -3,7 +3,5 @@ public static class CreateRoles
     public static void AddRole()
     {
         string roleName = PresentationHelper.GetString("What is the name of the role? ", "role");
-
-
     }
 }
