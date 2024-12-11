02ed21aa557a610fecde297ae557e2761969c7b3
Mvdv00 <matthijsvorm@gmail.com>
2024-12-11 10:05:57 +0100

    role unittests

--- UnitTests/WeekScheduleTest.cs
+++ UnitTests/WeekScheduleTest.cs
@@ -1,30 +1,5 @@
 // namespace UnitTests;
 
-<<<<<<< Updated upstream
-[TestClass]
-public class MovieLogicTests
-{
-    [TestMethod]
-    public void IsAvailable_ReturnsTrue_WhenRoomIsAvailable()
-    {
-        int room = 1;
-        DateTime startTime = new DateTime(2024, 11, 25, 10, 0, 0);
-        DateTime endTime = startTime.AddHours(2); 
-
-        Assert.IsTrue(MovieLogic.IsAvailable(room, startTime, endTime));
-    }
-
-    [TestMethod]
-    public void IsAvailable_ReturnsFalse_WhenRoomIsUnavailable()
-    {
-        int room = 2;
-        DateTime startTime = new DateTime(2024, 11, 25, 14, 0, 0); 
-        DateTime endTime = startTime.AddHours(2); 
-
-        Assert.IsFalse(MovieLogic.IsAvailable(room, startTime, endTime));
-    }
-}
-=======
 // [TestClass]
 // public class MovieLogicTests
 // {
@@ -50,4 +25,3 @@ public class MovieLogicTests
 //         Assert.IsFalse(ScheduleAccess.IsAvailable(room, startTime, endTime, (int)location.Id));
 //     }
 // }
->>>>>>> Stashed changes
