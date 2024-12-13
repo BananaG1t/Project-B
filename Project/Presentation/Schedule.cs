2265d6a1e9051ac678771b46a87a711b668dba4c
BananaG1t <bananamain@riseup.net>
2024-12-13 14:13:27 +0100

    working version

--- Project/Presentation/Schedule.cs
+++ Project/Presentation/Schedule.cs
@@ -19,18 +19,46 @@ static class Schedule
 
     public static void DisplaySchedule(LocationModel location)
     {
-        Console.Clear();
+        List<ScheduleModel> schedules = ScheduleAccess.ScheduleByDateAndLocation(location);
+        var scheduleByDay = ScheduleLogic.GroupByDay(schedules);
+        int dayIndex = 0;
+        
+        int input = 0;
 
-        List<ScheduleModel> Schedules = ScheduleAccess.ScheduleByDateAndLocation(location);
+        while (input != 3)
+        {
+            Console.Clear();
+            List<int> valid = [3];
+            string text = $"Movies Playing on {scheduleByDay[dayIndex].Key:dd-MM-yy}";
+            foreach (ScheduleModel schedule in scheduleByDay[dayIndex])
+            {
+                text += $"\nMovie: {schedule.Movie.Name}, Room: {schedule.Auditorium.Room}, Starting time: {schedule.StartTime:HH:mm}";
+            }
+            
+            text += "\n";
+
+            if (dayIndex != scheduleByDay.Count - 1)
+            {
+                valid.Add(1);
+                text += "[1] Next\n";
+            }
+            if (dayIndex != 0)
+            {
+                valid.Add(2);
+                text += "[2] Previous\n";
+            }
+
+            text += "[3] Back";
+            input = PresentationHelper.MenuLoop(text, valid);
+            
+            if (input == 1)
+                dayIndex++;
+            if (input == 2)
+                dayIndex--;
+        }
 
         // Shows what movie are playing based on the date and time and location
-        Console.WriteLine($"Movies Playing");
-        foreach (ScheduleModel schedule in Schedules)
-        {
-            Console.WriteLine(@$"Movie: {schedule.Movie.Name}, 
-Room: {schedule.Auditorium.Room}, 
-Starting time: {schedule.StartTime.ToString("dd-MM-yyyy HH:mm")}");
-        }
-        Console.WriteLine();
+       
+        Console.Clear();
     }
 }
