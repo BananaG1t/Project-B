1bc4297e730de12d8f87879e380cbae11193913d
BananaG1t <bananamain@riseup.net>
2025-01-06 14:17:02 +0100

    filtered schedule selection by day

--- Project/Presentation/Schedule.cs
+++ Project/Presentation/Schedule.cs
@@ -2,19 +2,50 @@ static class Schedule
 {
     public static ScheduleModel SelectSchedule(LocationModel location)
     {
-        List<ScheduleModel> Schedules = ScheduleAccess.ScheduleByDateAndLocation(location);
+        List<ScheduleModel> schedules = ScheduleAccess.ScheduleByDateAndLocation(location);
+        var scheduleByDay = ScheduleLogic.GroupByDay(schedules);
+        int dayIndex = 0;
 
-        string text = "what schedule do you want to see";
-
-        for (int i = 0; i < Schedules.Count; i++)
+        int input = -1;
+        while (input < 3 && input != 0)
         {
-            text += $"\n[{i + 1}] Movie: {Schedules[i].Movie.Name}, Room: {Schedules[i].Auditorium.Room}, Starting time: {Schedules[i].StartTime}";
+            Console.Clear();
+            List<int> valid = [0];
+            string text = $"Movies Playing on {scheduleByDay[dayIndex].Key:dd-MM-yy}";
 
+            if (dayIndex != scheduleByDay.Count - 1)
+            {
+                valid.Add(1);
+                text += "\n[1] Next day";
+            }
+
+            if (dayIndex != 0)
+            {
+                valid.Add(2);
+                text += "\n[2] Previous day";
+            }
+
+            int cnt = 0;
+            foreach (ScheduleModel schedule in scheduleByDay[dayIndex])
+            {
+                text += $"\n[{cnt + 3}] Movie: {schedule.Movie.Name}, Room: {schedule.Auditorium.Room}, Starting time: {schedule.StartTime}";
+                valid.Add(cnt++ + 3);
+            }
+
+
+
+            text += "\n[0] Back";
+            input = PresentationHelper.MenuLoop(text, valid);
+
+            if (input == 0)
+                return null;
+            if (input == 1)
+                dayIndex++;
+            if (input == 2)
+                dayIndex--;
         }
 
-        int input = PresentationHelper.MenuLoop(text, 1, Schedules.Count + 1);
-
-        return Schedules[input - 1];
+        return schedules[input - 3];
     }
 
     public static void DisplaySchedule(LocationModel location)
@@ -22,7 +53,7 @@ static class Schedule
         List<ScheduleModel> schedules = ScheduleAccess.ScheduleByDateAndLocation(location);
         var scheduleByDay = ScheduleLogic.GroupByDay(schedules);
         int dayIndex = 0;
-        
+
         int input = 0;
 
         while (input != 3)
@@ -34,23 +65,21 @@ static class Schedule
             {
                 text += $"\nMovie: {schedule.Movie.Name}, Room: {schedule.Auditorium.Room}, Starting time: {schedule.StartTime:HH:mm}";
             }
-            
-            text += "\n";
 
             if (dayIndex != scheduleByDay.Count - 1)
             {
                 valid.Add(1);
-                text += "[1] Next\n";
+                text += "\n[1] Next";
             }
             if (dayIndex != 0)
             {
                 valid.Add(2);
-                text += "[2] Previous\n";
+                text += "\n[2] Previous";
             }
 
-            text += "[3] Back";
+            text += "\n[3] Back";
             input = PresentationHelper.MenuLoop(text, valid);
-            
+
             if (input == 1)
                 dayIndex++;
             if (input == 2)
@@ -58,7 +87,7 @@ static class Schedule
         }
 
         // Shows what movie are playing based on the date and time and location
-       
+
         Console.Clear();
     }
 }
