36b08c16b2e5422bca7acdb6504bab32f75e0125
BananaG1t <bananamain@riseup.net>
2025-01-08 11:29:07 +0100

    Update SnacksModel.cs

--- Project/DataModels/SnacksModel.cs
+++ Project/DataModels/SnacksModel.cs
@@ -1,20 +1,14 @@
-using System.Data.Common;
-
 public class SnacksModel
 {
     public string Name { get; set; }
     public double Price { get; set; }
     public int Id { get; set; }
-    public DateTime Date { get; set; }
-    public int MovieId { get; set; }
-    public int ReservationId { get; set; }
-    public double Amount { get; set; }
 
-    public SnacksModel(Int64 id ,string name, double price)
+    public SnacksModel(Int64 id, string name, double price)
     {
         Id = (int)id;
         Name = name;
-        Price = price; 
+        Price = price;
     }
 
     public SnacksModel(string name, double price)
