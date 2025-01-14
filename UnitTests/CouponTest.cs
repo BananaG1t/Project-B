8da68873e07b5d2a25f86a57790636d889c704b2
Jules-sudo <1084973@hr.nl>
2025-01-14 10:14:54 +0100

    Update CouponTest.cs

---
+++ UnitTests/CouponTest.cs
@@ -0,0 +1,42 @@
+namespace UnitTests;
+
+[TestClass]
+public class TestRoles
+{
+    [TestMethod]
+    [DataRow("staff", true)]
+    [DataRow("admin", true)]
+    [DataRow("floor manager", true)]
+    [DataRow("user", false)]
+    [DataRow("watermeloen", false)]
+    [DataRow("kip", false)]
+    [DataRow("stevie wonder", false)]
+    public void StaffRoleInDatabase(string roleName, bool expected)
+    {
+        RoleModel role = RoleAccess.GetByName(roleName);
+
+        Assert.AreEqual(role != null, expected);
+    }
+
+    [TestMethod]
+    [DataRow(true)]
+    public void LocationIdCheck(bool expected)
+    {
+        List<AssignedRoleModel> assignedRoles = AssignedRoleAccess.GetAllAssignedRoles();
+        foreach (AssignedRoleModel assignedRole in assignedRoles)
+        {
+            Assert.AreEqual(assignedRole.LocationId is long, expected);
+        }
+    }
+
+    [TestMethod]
+    [DataRow("Display income overview", "staff", true)]
+    [DataRow("Display income overview", "floor manager", false)]
+    [DataRow("Display income overview", "admin", false)]
+
+    public void StaffRoleCgeck(string roleLevelName, string roleName, bool expected)
+    {
+        RoleModel role = RoleAccess.GetByName(roleName);
+        RoleLevelModel roleLevel = RoleLevelAccess.GetByFunctionality(roleLevelName);
+        Assert.AreEqual(role.LevelAccess < roleLevel.LevelNeeded, expected);
+    }
