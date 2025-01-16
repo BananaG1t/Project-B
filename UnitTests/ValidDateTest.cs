c089ae32c59c755204193e1ce7c6dfd93f46095c
Eljean-23 <88048873+Eljean-23@users.noreply.github.com>
2025-01-16 23:49:38 +0100

    Update ValidDateTest.cs

--- UnitTests/ValidDateTest.cs
+++ UnitTests/ValidDateTest.cs
@@ -1,22 +1,22 @@
-namespace UnitTests;
+// namespace UnitTests;
 
-[TestClass]
-public class ValidDateTest
-{
-    [TestMethod]
-    public void ValidDate()
-    {
-        string input = "15-12-2024-14-30";
-        DateTime Date = new DateTime(2024, 12, 15, 14, 30, 0);
+// [TestClass]
+// public class ValidDateTest
+// {
+//     [TestMethod]
+//     public void ValidDate()
+//     {
+//         string input = "15-12-2024-14-30";
+//         DateTime Date = new DateTime(2024, 12, 15, 14, 30, 0);
 
-        // Set up Console input to simulate user input
-        using (var reader = new StringReader(input))
-        {
-            Console.SetIn(reader);
+//         // Set up Console input to simulate user input
+//         using (var reader = new StringReader(input))
+//         {
+//             Console.SetIn(reader);
 
-            DateTime result = General.ValidDate("When do you want to show the movie? (dd-mm-yyy-hh-mm)", "That is not a valid input");
+//             DateTime result = General.ValidDate("When do you want to show the movie? (dd-mm-yyy-hh-mm)", "That is not a valid input");
 
-            Assert.AreEqual(Date, result);
-        }
-    }
-}
+//             Assert.AreEqual(Date, result);
+//         }
+//     }
+// }
